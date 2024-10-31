9e738810f97cd48f425683279fa1062c0ca12a79
dutra791 <dutradiego791@gmail.com>
2024-10-31 22:31:21 +0000

    31/10

---
+++ dutra-agenda-app/app/Models/Evento.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model
+{
+    use HasFactory;
+
+    //Listar os campos que o Model vai trabalhar
+    //Serve tambem para criação em massa de dados
+    protected $fillable = ['data','descricao','inicio', 'final',
+    'contato','realizado'];
+
+    // Relacionamentos
+    //Policies
+    
+
+
+}
--- dutra-agenda-app/database/migrations/0001_01_01_000000_create_users_table.php
+++ dutra-agenda-app/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            //DATA DO EVENTO: tipo data
+            $table->date('data');
+            //DESCRIÇAO:TEXTO
+            $table->text('descricao');
+            //HORA INICIAL: HORA
+            $table->time('inicio');
+            //HORA FINAL: HORA
+            $table->time('final');
+            //CONTATO
+            $table->string('contato');
+            //REALIZADO: BOOLEANO
+            $table->boolen('realizado');
             $table->timestamps();
         });
 
